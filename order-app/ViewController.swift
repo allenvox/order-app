0b92ac7418c5484401fadbf753f6d558d1e44203
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-10-15 11:21:22 +0700

    Initial Commit

---
+++ order-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  order-app
+//
+//  Created by Yuriy Grigoryev on 15.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
